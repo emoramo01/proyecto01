7f316148542468003147f46cc42654375d83e511
profesor <profesor@DESKTOP-2C657IO>
2022-02-10 09:09:38 +0100

    03:08:09

--- Proyecto01/src/paquete01/Clase01.java
+++ Proyecto01/src/paquete01/Clase01.java
@@ -3,4 +3,6 @@ package paquete01;
 public class Clase01 {
 // primer commit
 // seguno commit
+	// tercer commit
+	
 }
--- Proyecto01/src/paquete01/Clase03.java
+++ Proyecto01/src/paquete01/Clase03.java
@@ -5,7 +5,7 @@ public class Clase03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// primer commit
-		// tercer commit
+		// tercer commit repe
 	}
 
 }
